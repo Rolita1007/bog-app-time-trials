1d49332b8387e581d8e4b004886d53aabb32c206
rolita1007 <rolita1007@gmail.com>
2017-07-05 14:39:38 -0400

    added new show create also view html

--- app/controllers/creatures_controller.rb
+++ app/controllers/creatures_controller.rb
@@ -4,4 +4,26 @@ class CreaturesController < ApplicationController
     @creatures = Creature.all
   end
 
+  def new
+    @creature = Creature.new
+  end
+
+  def create
+    @creature = Creature.new(creature_params)
+    if @creature.save!
+      redirect_to creatures_path
+    end
+  end
+
+  def show
+    @creature = Creature.find(params[:id])
+  end
+
+
+
+  private
+  def creature_params
+    params.require(:creature).permit(:name, :description)
+  end
+
 end
